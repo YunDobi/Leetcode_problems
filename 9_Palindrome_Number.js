0b855a0fed76f03a0b300f77f328be75f9b440b4
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-03-05 22:01:54 -0800

    first commit

---
+++ 9_Palindrome_Number.js
@@ -0,0 +1,21 @@
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+const isPalindrome = function(x) {
+  let array_item = Array.from(String(x), Number);
+  let reversed_array = [];
+  for (let i = array_item.length - 1; i >= 0; i--) {
+    reversed_array.push(array_item[i]);
+  }
+  if (array_item.join('') === reversed_array.join('')) {
+    return true;
+  } else {
+    return false;
+  }
+  
+
+
+};
+
+console.log(isPalindrome(121));
