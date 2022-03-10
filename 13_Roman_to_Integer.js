bed713bd50328c0172685d04a15768bbf2a276d3
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-03-09 21:33:02 -0800

    solution for 13

--- 13_Roman_to_Integer.js
+++ 13_Roman_to_Integer.js
@@ -2,52 +2,48 @@
  * @param {string} s
  * @return {number}
  */
- const romanToInt = function(s) {
+const romanToInt = function(s) {
   let symbolObj = {
-      "I": 1,
-      "V": 5,
-      "X": 10,
-      "L": 50,
-      "C": 100,
-      "D": 500,
-      "M": 1000
-  }
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
   let given = s.split("");
   let result = 0;
   for (let i = 0; i < given.length; i++) {
-      let letter = given[i]
-      console.log(given[i],symbolObj[letter])
+    let letter = given[i];
       
-      if (letter === "I") {
-          
-          if (given[i + 1] === "V" || given[i + 1] === "X") {
-              result += (symbolObj[given[i + 1]] - symbolObj[letter])
-              i++
-          } else {
-              result += symbolObj[letter]
-          }
+    if (letter === "I") {
+      if (given[i + 1] === "V" || given[i + 1] === "X") {
+        result += (symbolObj[given[i + 1]] - symbolObj[letter])
+        i++;
+      } else {
+        result += symbolObj[letter]
       }
-      if (letter === "C") {
-          if (given[i + 1] === "D" || given[i + 1] === "M") {
-              result += symbolObj[given[i + 1]] - symbolObj[letter]
-              console.log(result)
-              i++
-          } else {
-              result += symbolObj[letter]
-          }
-      }  
-      if (letter === "X") {
-          if (given[i + 1] === "L" || given[i + 1] === "C") {
-              result += symbolObj[given[i + 1]] - symbolObj[letter]
-              i++
-          } else {
-              result += symbolObj[letter]
-          }
+    }
+    if (letter === "C") {
+      if (given[i + 1] === "D" || given[i + 1] === "M") {
+        result += symbolObj[given[i + 1]] - symbolObj[letter];
+        i++;
+      } else {
+        result += symbolObj[letter];
       }
-      if (letter === "V" || letter === "L" || letter === "D" || letter === "M") {
-          result += symbolObj[letter]
+    }
+    if (letter === "X") {
+      if (given[i + 1] === "L" || given[i + 1] === "C") {
+        result += symbolObj[given[i + 1]] - symbolObj[letter]
+        i++;
+      } else {
+        result += symbolObj[letter];
       }
-      console.log("result:",result)
+    }
+    if (letter === "V" || letter === "L" || letter === "D" || letter === "M") {
+      result += symbolObj[letter];
+    }
   }
-  return result
+  return result;
 };
