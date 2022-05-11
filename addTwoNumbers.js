84ca035a8c72e6c3a82c07f02e87fc811cd8c62c
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-05-11 16:54:50 -0700

    second submit from solutions

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -5,46 +5,65 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-// /**
-//  * @param {ListNode} l1
-//  * @param {ListNode} l2
-//  * @return {ListNode}
-//  */
 
-//currently what I sumbit first. However, if the different length of the array, return short version.
-const addTwoNumbers = function(l1, l2) {
-  let result = [];
-  let sum = 0;
-  let addingResult = function () {
-   if (!l1 || !l2) {
-      return;
-      }
-      let total = l1.val + l2.val + sum
-      // console.log("each",total)
-      l1 = l1.next, l2= l2.next
-      if (total > 9) {
-          result.push(0)
-          sum += 1
-      } else {
-         result.push(total)
-          sum = 0
-      }
-      // console.log(total)
-      addingResult(l1, l2)   
-  }
-  addingResult()
-  console.log(result)
+// currently what I sumbit first. However, if the different length of the array, return short version.
+// const addTwoNumbers = function(l1, l2) {
+//   let result = [];
+//   let sum = 0;
+//   let addingResult = function () {
+//    if (!l1 || !l2) {
+//       return;
+//       }
+//       let total = l1.val + l2.val + sum
+//       // console.log("each",total)
+//       l1 = l1.next, l2= l2.next
+//       if (total > 9) {
+//           result.push(0)
+//           sum += 1
+//       } else {
+//          result.push(total)
+//           sum = 0
+//       }
+//       // console.log(total)
+//       addingResult(l1, l2)   
+//   }
+//   addingResult()
+//   console.log(result)
   
   
-  let head = result.reverse().reduce((acc, curr) => {
-if (acc == null) {
-  acc = new ListNode(curr);
+//   let head = result.reverse().reduce((acc, curr) => {
+// if (acc == null) {
+//   acc = new ListNode(curr);
 
-} else {
-  acc = new ListNode(curr, acc);
-}
-return acc;
-}, null);
-  return(head)
+// } else {
+//   acc = new ListNode(curr, acc);
+// }
+// return acc;
+// }, null);
+//   return(head)
   
+// };
+
+//fro the sumbition
+let carry = 0;
+
+let addTwoNumbers = function(l1, l2) {
+    if(!l1 && !l2 && carry === 0) {
+        return null;
+    }
+    
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
+    let nextL1 = l1 ? l1.next : null;
+    let nextL2 = l2 ? l2.next : null;
+    
+    let sum = x + y + carry;
+
+    carry = Math.floor(sum/10);
+    sum = sum % 10;
+        
+    return {
+        val: sum,
+        next: addTwoNumbers(nextL1, nextL2)
+    };
 };
